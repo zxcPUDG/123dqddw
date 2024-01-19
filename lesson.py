a4e0bea07ba49b1c331f227e5e169b879fc8a23c
adel-gain-2007@yandex.ru <adel-gain-2007@yandex.ru>
2024-01-19 17:41:14 +0300

    123 adel

---
+++ lesson.py
@@ -0,0 +1,75 @@
+# Hello=2
+# world=3
+# print(Hello,world,'Hello world')
+# str()   #'qefqefqfqfqfq'
+# int()   #2 4 6
+# float() #2.3
+# list() #['21','25','15']
+
+# list=['44','21','62']
+# list[0]=int(list[0])
+# list[2]=int(list[2])
+# print(list[0]+list[2], '+')
+# print(list[0]-list[2], '-')
+# print(list[0]*list[2], '*')
+# print(list[0]/list[2], '/') 
+
+# if 15>=20:
+# 	print('true')
+# else:
+# 	print('false')
+# print('do you like play on pudge???')
+# cart=[]
+# eggs=True
+# sweet=False
+# bread=True
+# if bread==True:
+# 	cart.append('bread')
+# #print(cart)
+# if eggs==True:
+#     cart.append('eggs')
+#     cart.append('eggs')
+#     cart.append('eggs')
+#     cart.append('eggs')
+#     cart.append('eggs')
+#     cart.append('eggs')
+#     cart.append('eggs')
+#     cart.append('eggs')
+#     cart.append('eggs')
+# if sweet==True:
+# 	cart.append('sweet')
+# 	cart.append('sweet')
+# 	cart.append('sweet')
+# 	cart.append('sweet')
+# 	cart.append('sweet')
+# print(cart)	
+
+login=['shadow fiend' ,'pudge' ,'zxc']
+password=['ezmid','my dick is big','igra proebana opyat(']
+your_login=input('login:')
+if your_login==login[0]:
+	your_password=input('password:')
+	if your_password==password[0]:
+		print('success')
+	else:
+		print('password incorrect')
+else:
+	print('login incorrect')
+
+if your_login==login[1]:
+	your_password=input('password:')
+	if your_password==password[1]:
+		print('success')
+	else:
+		print('password incorrect')
+else:
+	print('login incorrect')
+
+if your_login==login[2]:
+	your_password=input('password:')
+	if your_password==password[2]:
+		print('success')
+	else:
+		print('password incorrect')
+else:
+	print('login incorrect')
